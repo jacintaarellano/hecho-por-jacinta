a0121ef71e6ede586be600b012e97f81d7ff3d68
Jacinta-Arellano <jacinta.arellano.r@mail.pucv.cl>
2026-01-17 23:37:40 -0300

    actualizacion de home
    js para slider en fotos del landing y suma de un producto destacado

---
+++ script.js
@@ -0,0 +1,13 @@
+// Carrusel home
+console.log("JS conectado");
+
+let index = 0;
+const slides = document.querySelectorAll(".slide");
+
+function showSlide() {
+  slides.forEach(slide => slide.classList.remove("active"));
+  slides[index].classList.add("active");
+  index = (index + 1) % slides.length;
+}
+
+setInterval(showSlide, 4000);
