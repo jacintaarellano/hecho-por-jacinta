55e0348323facf0ce129c361f1e160a40673df7e
Jacinta-Arellano <jacinta.arellano.r@mail.pucv.cl>
2026-01-18 01:42:28 -0300

    actualizacion productos y fundas

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-// Carrusel home
 console.log("JS conectado");
 
-let index = 0;
 const slides = document.querySelectorAll(".slide");
+let index = 0;
 
-function showSlide() {
-  slides.forEach(slide => slide.classList.remove("active"));
-  slides[index].classList.add("active");
-  index = (index + 1) % slides.length;
+if (slides.length > 0) {
+  slides[0].classList.add("active");
+
+  setInterval(() => {
+    slides[index].classList.remove("active");
+    index = (index + 1) % slides.length;
+    slides[index].classList.add("active");
+  }, 3000);
 }
-
-setInterval(showSlide, 4000);
